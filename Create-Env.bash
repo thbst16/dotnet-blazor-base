bf8f5faf5443c40af9a17163ac32d84e45d1a355
Thomas Beck <thomas@beckshome.com>
2024-09-14 15:14:26 +0000

    updated env scripts

--- Create-Env.bash
+++ Create-Env.bash
@@ -2,14 +2,14 @@
 
 group="beckshome-container-apps-rg"
 location="eastus"
-env="dotnet-blazor-base-env"
+env="dotnet-blazor-env"
 app="dotnet-blazor-base"
 image="thbst16/dotnet-blazor-base:latest"
 
-az group create --name $group --location $location
-echo
-az containerapp env create -n $env -g $group --location $location
-echo
+# az group create --name $group --location $location
+# echo
+# az containerapp env create -n $env -g $group --location $location
+# echo
 az containerapp create -n $app -g $group --image $image --environment $env --min-replicas 0
 echo
 az containerapp ingress enable -n $app -g $group --type external --target-port 0 --transport auto
--- Delete-Env.bash
+++ Delete-Env.bash
@@ -4,5 +4,5 @@ group="beckshome-container-apps-rg"
 app="dotnet-blazor-base"
 
 az containerapp delete -g $group -n $app
-echo
-az group delete --name $group
+# echo
+# az group delete --name $group
